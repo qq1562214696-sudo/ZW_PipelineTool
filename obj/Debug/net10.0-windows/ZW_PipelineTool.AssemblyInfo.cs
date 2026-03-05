47cf5d7aad2bea0c32f0934583fd52eef8fd0cba
qq1562214696-sudo <qq1562214696@gmail.com>
2026-03-05 16:39:41 +0800

    最新

--- obj/Debug/net10.0-windows/ZW_PipelineTool.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/ZW_PipelineTool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZW_PipelineTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45a8520a4a2ceaa0e43576c65b6bc929e0645d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+825f245bc1b728588188ba5362bacf3d528f268f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZW_PipelineTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZW_PipelineTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
